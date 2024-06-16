0991146a8ba91eeea53affe93f3ca19c62160609
danixDe <150506676+danixDe@users.noreply.github.com>
2024-06-16 23:28:23 +0530

    drop down done

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -13,41 +13,68 @@ function DropDownItem({ text }) {
 }
 
 function Header() {
-    const [open, setOpen] = useState(false);
+    const [openProfile, setOpenProfile] = useState(false);
+    const [openPost, setOpenPost] = useState(false);
+    const[openDots, setOpenDots] = useState(false)
     return (
+        <>
         <div className={styles.x}>
             <div className={styles.name}>gvpBlind</div>
             <div className={styles.options}>
                 <div className={styles.wrapper}>
-                    <div className={styles.button} onClick={() => { setOpen((prev)=>!prev); }}>
+                    <div className={styles.button} onClick={() => { setOpenProfile(!openProfile); }}>
                         <img className={styles.usericon} src={logo} alt='user' />
                     </div>
-                    { 
-
-                    
-                    <div className={styles.dropdownmenu} >
-                        <ul>
-                            <DropDownItem text='my profile' />
-                            <DropDownItem text='settings' />
-                            <DropDownItem text='Logout' />
-                        </ul>
-                    </div>
-}
+                   
                 </div>
                 <div className={styles.wrapper}>
-                    <div className={styles.button}>
+                    <div className={styles.button} onClick = {()=>{setOpenPost(!openPost)}}>
                         <img className={styles.post} src={post} alt='post' />
                     </div>
                 </div>
                 <div className={styles.wrapper}>
-                    <div className={styles.button}>
+                    <div className={styles.button} onClick = {()=>{setOpenDots(!openDots)}}>
                         <img className={styles.dots} src={dots} alt='about' />
                     </div>
                 </div>
+             
             </div>
+            
+         
         </div>
-
+           {openProfile && (
+            <div className={styles.dropdownprofile}>
+                <ul>
+                    <DropDownItem text='Profile' />
+                    <DropDownItem text='Settings' />
+                    <DropDownItem text='Logout' />
+                </ul>
+            </div>
+        )}
+            {openPost && (
+            <div className={styles.dropdownpost}>
+                <ul>
+                    <DropDownItem text='New Post' />
+                    <DropDownItem text='Edit Post'/>
+                    <DropDownItem text='Delete Post'/>
+                </ul>
+            </div>
+        )}
+            {openDots && (
+            <div className={styles.dropdowndots}>
+                <ul>
+                    <DropDownItem text='Aboout' />
+                    <DropDownItem text='Privacy policy' />
+                    <DropDownItem text='License' />
+                </ul>
+            </div>
+        )}
+        
+        
+        </>
     );
+
+        
 }
 
 export default Header;
