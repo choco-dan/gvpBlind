7003b881cc5943e1e9568da389be4ab17405e9d8
danixDe <150506676+danixDe@users.noreply.github.com>
2024-06-18 23:18:26 +0530

    dropdown fully functional

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,80 +1,108 @@
-import React, { useState } from 'react';
+import React, { useState, useRef} from 'react';
 import styles from './header.module.css';
 import logo from './assets/user.svg';
 import post from './assets/script.svg';
 import dots from './assets/menu-dots.svg';
 
-function DropDownItem({ text }) {
-    return (
-        <li className={styles.dropdownitem}>
-            <a>{text}</a>
-        </li>
-    );
-}
-
 function Header() {
+    const profileMenu = ['PROFILE','SETTINGS','LOGOUT'];
+    const postMenu = ['POSTS','LIKES','COMMENTS'];
+    const dotsMenu = ['ABOUT','POLICY','LICENSE'];
     const [openProfile, setOpenProfile] = useState(false);
     const [openPost, setOpenPost] = useState(false);
-    const[openDots, setOpenDots] = useState(false)
+    const[openDots, setOpenDots] = useState(false);
+
+   const profileMenuRef = useRef();
+   const profileRef = useRef();
+   const postMenuRef = useRef();
+   const postRef = useRef();
+   const dotsMenuRef = useRef();
+   const dotsRef = useRef();
+
+   window.addEventListener('click', (e)=>{
+   if(e.target !== profileMenuRef.current && e.target !== profileRef.current){
+        setOpenProfile(false);
+    }})
+    window.addEventListener('click',(e)=>{
+         if(e.target !== postMenuRef.current && e.target !== postRef.current){
+            setOpenPost(false);
+        }})
+    window.addEventListener('click',(e)=>{
+        if(e.target !== dotsMenuRef.current && e.target !== dotsRef.current){
+            setOpenDots(false);
+        }
+    })
+    
+
     return (
         <>
         <div className={styles.x}>
             <div className={styles.name}>gvpBlind</div>
             <div className={styles.options}>
-                <div className={styles.wrapper}>
-                    <div className={styles.button} onClick={() => { setOpenProfile(!openProfile); }}>
-                        <img className={styles.usericon} src={logo} alt='user' />
+                    <div
+                    className={styles.button}
+                     onClick={() => { setOpenProfile(!openProfile);}}>
+                        <img  ref = {profileRef}
+                         className={styles.usericon} src={logo} alt='user' />
                     </div>
-                   
+                    <div 
+                     className={styles.button}
+                      onClick = {()=>{setOpenPost(!openPost);}}>
+                        <img ref = {postRef}
+                        className={styles.post} src={post} alt='post' />
+                    </div>              
+                    <div
+                     className={styles.button}
+                      onClick = {()=>{setOpenDots(!openDots);}}>
+                        <img ref = {dotsRef}
+                        className={styles.dots} src={dots} alt='about' />
+                    </div>     
                 </div>
-                <div className={styles.wrapper}>
-                    <div className={styles.button} onClick = {()=>{setOpenPost(!openPost)}}>
-                        <img className={styles.post} src={post} alt='post' />
-                    </div>
-                </div>
-                <div className={styles.wrapper}>
-                    <div className={styles.button} onClick = {()=>{setOpenDots(!openDots)}}>
-                        <img className={styles.dots} src={dots} alt='about' />
-                    </div>
-                </div>
-             
             </div>
-            
-         
-        </div>
-           {openProfile && (
-            <div className={styles.dropdownprofile}>
-                <ul>
-                    <DropDownItem text='Profile' />
-                    <DropDownItem text='Settings' />
-                    <DropDownItem text='Logout' />
-                </ul>
-            </div>
-        )}
-            {openPost && (
-            <div className={styles.dropdownpost}>
-                <ul>
-                    <DropDownItem text='New Post' />
-                    <DropDownItem text='Edit Post'/>
-                    <DropDownItem text='Delete Post'/>
-                </ul>
-            </div>
-        )}
-            {openDots && (
-            <div className={styles.dropdowndots}>
-                <ul>
-                    <DropDownItem text='Aboout' />
-                    <DropDownItem text='Privacy policy' />
-                    <DropDownItem text='License' />
-                </ul>
-            </div>
-        )}
         
-        
-        </>
-    );
+        {openProfile && (
+                     <div ref = {profileMenuRef}
+                     className = {styles.dropdownprofile}>
+                     <ul>
+                         {profileMenu.map((menu,index)=>(
+                             <li onClick={()=>setOpenProfile(false)}
+                               className = {styles.dropdownitem} key={index}>
+                                 {menu}      
+                             </li>
+                         ))}
+                     </ul>
+                     </div> 
 
-        
+                )};
+        {openPost && (
+                    <div ref = {postMenuRef}
+                     className = {styles.dropdownpost}>
+                    <ul>
+                        {postMenu.map((menu,index)=>(
+                            <li onClick = {()=>setOpenPost(false)}
+                             className= {styles.dropdownitem} key = {index}>
+                                {menu}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                )};
+        {openDots && (
+                    <div ref = {dotsMenuRef}
+                     className = {styles.dropdowndots}>
+                    <ul>
+                        {dotsMenu.map((menu,index)=>(
+                            <li onClick={()=>setOpenDots(false)}
+                            className = {styles.dropdownitem}
+                            key = {index}>
+                                {menu}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                )};
+        </>
+    );   
 }
 
 export default Header;
